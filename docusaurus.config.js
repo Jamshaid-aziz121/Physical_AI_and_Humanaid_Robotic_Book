74d427985f6e9d93c2edb775cca482e6d83e6973
jamshaid <jamshaid.aziz121@gmail.com>
2026-01-09 22:52:47 +0500

    fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jamshaid-aziz121.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'Physical_AI_and_Humanaid_Robotic_Book/physical-ai-humanoid-book',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
